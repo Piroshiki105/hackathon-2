9b73bb2f5a064fdbd2ffbca75385e573eac8b369
matsumoto.hirosato <matsumoto.hirosato@interfactory.co.jp>
2019-08-03 15:45:25 +0900

    漏れ

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var rrd = require('react-router-dom');
 var Link = rrd.Link;
+var Const = require('./Const')
 
 // 一覧レンダリング用コンポーネント
 class Chat extends React.Component {
