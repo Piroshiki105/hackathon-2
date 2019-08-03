432f2c02d95fee739955872477dc24104c51ad50
yanagi.kento <yanagi.test.kento@gmail.com>
2019-08-03 14:19:06 +0900

    pgp#anyの実引数を修正

--- backend/controllers/link_controller.js
+++ backend/controllers/link_controller.js
@@ -4,23 +4,12 @@ var pgp = require('pg-promise')(/* options */);
 var db = pgp('postgres://postgres:Yanagi2250@127.0.0.1:5432/hackathon2');
 const BASE_URL = 'http://localhost:3000'
 
-exports.all = (req, res, next) => {
-  db.any("select * from LINK limit 100")
+exports.fetch = (req, res, next) => {
+  db.any("select * from LINK limit $1 offset $2", [req.params.count, req.params.page])
     .then((data) => {
-      res.json({ apply_list: data });
+      res.json({ link_list: data });
     })
     .catch((error) => {
       console.log('ERROR:', error);
-    });
-};
-
-exports.findById = (req, res, next) => {
-  db.one("select * from LINK where ID = $1", req.params.id)
-    .then((data) => {
-      res.json({ apply: data });
-    })
-    .catch((error) => {
-      console.log('ERROR:', error);
-      res.json({});
     });
 };
