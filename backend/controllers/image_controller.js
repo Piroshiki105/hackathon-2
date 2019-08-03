861bbab6e88042fff6542ab6f7d01358e7a7e686
yanagi.kento <yanagi.test.kento@gmail.com>
2019-08-03 14:14:18 +0900

    image_controller.js 追加

---
+++ backend/controllers/image_controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var pgp = require('pg-promise')(/* options */);
+var db = pgp('postgres://postgres:Yanagi2250@127.0.0.1:5432/hackathon2');
+const BASE_URL = 'http://localhost:3000'
+
+exports.fetch = (req, res, next) => {
+  db.any("select * from LINK limit $1 offset $2", req.params.count, req.params.page)
+    .then((data) => {
+      res.json({ link_list: data });
+    })
+    .catch((error) => {
+      console.log('ERROR:', error);
+    });
+};
